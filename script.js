b035cdbb8a954b33eef796068cb7a5cffbe05a62
Adam Martinez <12470793+mart3318@users.noreply.github.com>
2023-06-22 14:54:36 -0500

    feat: Add Task object constructor

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log('Hello World')
+function Task(title, description) {
+    this.title = title
+    this.description = description
+    //this.dueDate = dueDate
+    //this.priority = priority
+}
+
+document.getElementById('task-form').addEventListener('submit', function(e) {
+    e.preventDefault();
+    let title = document.getElementById('title').value
+    let description = document.getElementById('description').value
+    let task = new Task(title, description)
+    console.log(task)
+})
